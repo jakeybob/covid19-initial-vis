eeb9b6a1f11ab28eeb4f48153b319e231887942a
Bob Taylor <j.boaby@gmail.com>
2020-03-02 21:16:03 +0000

    updates etc

--- README.md
+++ README.md
@@ -1,38 +1,68 @@
 # covid19-vis
 Some quick plots of data from the COVID-19 outbreak, data sourced from [here](https://github.com/CSSEGISandData/COVID-19).
 
+## Caveats
+
+* #not_an_epidemiologist
+* all data is provisional and subject to change
+* standards and definitions of e.g. "recovery" may differ in different countries
+* at this point, the time to recovery (or death) is comparable to the total observation time, so all data is subject to e.g. phase lag effects -- e.g. mortality rate will go down if an area has a sudden glut of confirmed cases, simply because those new cases are ongoing and have unknown recovery/mortality status
+* No error bars or confidence intervals are included for exactly this reason
+
 ## Cases by country
 
 * The cumulative confirmed cases in China, compared to the rest of the world.
 
 ![cumulative confirmed cases in China, and the rest of the world](pics/p_cases_country.png)
 
-* The cumulative confirmed cases in each affected country.
+* The cumulative confirmed cases in each affected country (note: *y* axes are independent)
 
 ![cumulative confirmed cases by country](pics/p_cases_country_facet.png)
 
 
 ## Rate of New Cases
 
-* The number of new confirmed cases per day in China, compared to the rest of the world.
+* The number of confirmed cases per day in China, compared to the rest of the world (note: *y* axes are independent)
 
 ![new confirmed cases per day in China, and the rest of the world](pics/p_cases_per_day_country.png)
 
-* The number of new confirmed cases per day (top six countries only)
+* The number of new confirmed cases per day (note: top six countries only, *y* axes are independent)
 
 ![](pics/p_cases_per_day_country_facet.png)
 
 
 ## Mortality and Recovery
 
-* Global mortality rate (left) and both global mortality and recovery rates (right).
+* Global mortality rate against time (left) and both global mortality and recovery rates against time (right). Where "mortality" and "recovery" rates are defined as (total deaths / total cases) and (total recoveries / total cases) at each date point respectively.
 
 ![](pics/p_both_mortality_recovered.png)
 
-* Mortality rates for each affected country.
+* Mortality rates for each affected country (note: *y* axes are independent)
 
 ![](pics/p_mortality_facet.png)
 
-* Recovery rates for each affected country.
+* Recovery rates for each affected country (note: *y* axes are independent)
 
 ![](pics/p_recovered_facet.png)
+
+* Mortality and recovery animated over time (top six countries only). Each dot represents a country, with the dot size proportional to the number of cases. The main dot (China) moves to the right over time, indicating more recovered cases, while the other countries are yet to settle into a stable pattern. Note that even China, on the most recent day available, still has only ~55% recovered and ~3% mortality, indicating >40% of known cases are ongoing.
+
+![](pics/anim_mortality_recovered.gif)
+
+## Spatial Evolution
+
+* On the left is the spherical weighted average of latitiude and longitude (where smallest area is recorded as e.g. Chinese province) of confirmed cases. As the majority of cases are still in Hubei, China, the "centre of mass" of the outbreak is still very much in China, having moved only ~2° west.
+* By transforming the latitudes and longitudes into spherical co-ordinates before averaging, we can obtain the average distance from the centre of the Earth, and use this as a first order proxy for weighted disease spread, i.e. here a value of 0 would indicate all the cases are in exactly the same place, and a value of 1 indicates equal spread over a full hemisphere.
+
+![](pics/p_com.png)
+
+## Maps
+
+* Just some straightforward maps showing number of cases with the most recent data.
+
+![](pics/map.png)
+
+![](pics/map2.png)
+
+
+
--- covid19.R
+++ covid19.R
@@ -27,11 +27,11 @@ df <- read_csv("data/covid19_confirmed.csv") %>%
               mutate(date = mdy(date))) %>%
   mutate_at(vars(starts_with("n_")), as.integer) %>%
   arrange(`Province/State`, `Country/Region`, Lat, Long) %>%
-  mutate(day = min(date) %--% date / days(1)) %>%  #  numbers of days since first case
+  mutate(day = min(date) %--% date / days(1)) %>%  #  numbers of days since first case
   group_by(`Province/State`, `Country/Region`, Lat, Long) %>%
   mutate(n_cases_per_day = if_else(row_number() == 1, 0, (n_cases - lag(n_cases)) / ((lag(date) %--% date) / days(1))),
-         n_deaths_per_day = if_else(row_number() == 1, 0,  (n_deaths - lag(n_deaths)) / ((lag(date) %--% date) / days(1))),
-         n_recovered_per_day = if_else(row_number() == 1, 0,  (n_recovered - lag(n_recovered)) / ((lag(date) %--% date) / days(1))) ) %>%
+         n_deaths_per_day = if_else(row_number() == 1, 0,  (n_deaths - lag(n_deaths)) / ((lag(date) %--% date) / days(1))),
+         n_recovered_per_day = if_else(row_number() == 1, 0,  (n_recovered - lag(n_recovered)) / ((lag(date) %--% date) / days(1))) ) %>%
   ungroup() %>%
   rename(sub_area = "Province/State", area = "Country/Region", lat = "Lat", long = "Long") %>%
   mutate(area = recode(area, "Mainland China" = "China",
@@ -62,11 +62,11 @@ p_cases_country <- df %>%
   group_by(area, date) %>% summarise(n_cases = sum(n_cases, na.rm=T)) %>%
   ggplot(aes(x = date, y = n_cases, colour=area)) +
   geom_point(size=dot_size) + geom_step(size=line_size) +
-  scale_y_continuous(labels=comma) + 
+  scale_y_continuous(labels=comma) +
   ggtitle("COVID-19 cumulative cases") + xlab("") + ylab("") +
   theme_custom + theme(legend.position = "bottom")
 
-p_cases_country
+# p_cases_country
 ggsave("pics/p_cases_country.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 
 
@@ -75,14 +75,15 @@ p_cases_country_facet <- df %>%
   group_by(area) %>% filter(sum(n_cases) > 0) %>%
   ggplot(aes(x = date, y = n_cases, group=area)) +
   geom_point(size=dot_size-2) + geom_step(size=line_size-1) +
-  scale_y_continuous(labels=comma) + 
+  scale_y_continuous(labels=comma) +
   facet_wrap(~area, scales = "free_y", labeller=label_wrap_gen(width = 20, multi_line = T)) +
   ggtitle("COVID-19 cumulative cases") + xlab("") + ylab("") +
   theme_custom + theme(axis.text.x = element_text(angle = 90),
                        axis.text.y = element_text(size=8))
 
-p_cases_country_facet
+# p_cases_country_facet
 ggsave("pics/p_cases_country_facet.png", device = "png", dpi="retina", width=300, height=200, units="mm")
+
 
 #### RATE OF NEW CASES ####
 
@@ -92,19 +93,20 @@ p_cases_per_day_country <- df %>%
   group_by(area) %>% filter(sum(n_cases_per_day) > 0) %>%
   ggplot(aes(x = date, y = n_cases_per_day, group=area, colour=area)) +
   geom_point(size=dot_size) + geom_line(size=line_size) +
-  scale_y_continuous(labels=comma) + 
+  scale_y_continuous(labels=comma) +
   facet_wrap(~area, scales = "free_y", labeller=label_wrap_gen(width = 20, multi_line = T)) +
   ggtitle("COVID-19 cases per day") + xlab("") + ylab("") +
   theme_custom + theme(axis.text.x = element_text(angle = 90),
                        axis.text.y = element_text(size=8),
                        legend.position = "none")
-p_cases_per_day_country
+# p_cases_per_day_country
 ggsave("pics/p_cases_per_day_country.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 
 
 top_6_most_recent <- df %>%
+  filter(area != "Others") %>%
   group_by(area, date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
-  group_by(area) %>% 
+  group_by(area) %>%
   filter(sum(n_cases_per_day) > 0,
          date == max(date)) %>%
   arrange(-n_cases_per_day) %>%
@@ -116,14 +118,14 @@ p_cases_per_day_country_facet <- df %>%
   filter(area %in% top_6_most_recent$area) %>%
   ggplot(aes(x = date, y = n_cases_per_day, group=area, colour=area)) +
   geom_point(size=dot_size-2) + geom_line(size=line_size-1) +
-  scale_y_continuous(labels=comma) + 
+  scale_y_continuous(labels=comma) +
   facet_wrap(~area, scales = "free_y", labeller=label_wrap_gen(width = 20, multi_line = T), ncol = 3) +
   ggtitle("COVID-19 cases per day") + xlab("") + ylab("") +
   theme_custom + theme(axis.text.x = element_text(angle = 90),
                        axis.text.y = element_text(size=8),
                        legend.position = "none")
 
-p_cases_per_day_country_facet
+# p_cases_per_day_country_facet
 ggsave("pics/p_cases_per_day_country_facet.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 # p_cases_per_day_country_facet$data <- p_cases_per_day_country_facet$data %>% ungroup()
 # plotly::ggplotly(p_cases_per_day_country_facet)
@@ -135,22 +137,22 @@ global_mortality <- sum(df$n_deaths, na.rm=T)/sum(df$n_cases, na.rm=T)
 p_global_mortality <- df %>%
   group_by(date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
   mutate(percent_mortality = n_deaths / n_cases,
-         percent_recovered = n_recovered / n_cases,) %>%
+         percent_recovered = n_recovered / n_cases) %>%
   ggplot(aes(x = date)) +
   geom_hline(yintercept = global_mortality, colour="red") +
   geom_point(aes(y = percent_mortality), size=dot_size) + geom_step(aes(y = percent_mortality), size=line_size) +
   scale_y_continuous(labels = percent_format(accuracy = .25)) +
-  annotate("text", label=paste0("", round(100*global_mortality, 2), "%"), 
+  annotate("text", label=paste0("", round(100*global_mortality, 2), "%"),
            x=max(df$date), y=global_mortality+5e-4, colour="red") +
   ggtitle("COVID-19 global mortality rate") + xlab("") + ylab("") +
   theme_custom
-p_global_mortality
+# p_global_mortality
 # ggsave("pics/p_global_mortality.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 
 p_global_mortality_recovered <- df %>%
   group_by(date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
   mutate(percent_mortality = n_deaths / n_cases,
-         percent_recovered = n_recovered / n_cases) %>% 
+         percent_recovered = n_recovered / n_cases) %>%
   pivot_longer(cols = starts_with("percent")) %>%
   ggplot(aes(x = date, y = value, group=name)) +
   geom_point(aes(colour=name), size=dot_size) + geom_step(aes(colour=name), size=line_size) +
@@ -158,7 +160,7 @@ p_global_mortality_recovered <- df %>%
   scale_color_hue(labels = c("mortality", "recovery")) +
   ggtitle("COVID-19 global mortality & recovery rates") + xlab("") + ylab("") +
   theme_custom + theme(legend.position = "bottom")
-p_global_mortality_recovered
+# p_global_mortality_recovered
 # ggsave("pics/p_global_mortality_recovered.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 
 p_both_mortality_recovered <- p_global_mortality + p_global_mortality_recovered
@@ -177,7 +179,7 @@ p_mortality_facet <- df %>%
   theme_custom + theme(axis.text.x = element_text(angle = 90),
                        axis.text.y = element_text(size=8),
                        legend.position = "none")
-p_mortality_facet
+# p_mortality_facet
 ggsave("pics/p_mortality_facet.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 
 p_recovered_facet <- df %>%
@@ -192,21 +194,125 @@ p_recovered_facet <- df %>%
   theme_custom + theme(axis.text.x = element_text(angle = 90),
                        axis.text.y = element_text(size=8),
                        legend.position = "none")
-p_recovered_facet
+# p_recovered_facet
 ggsave("pics/p_recovered_facet.png", device = "png", dpi="retina", width=300, height=200, units="mm")
 
 
-####
+#### MORTALITY / RECOVERED ANIMATION ####
 library(gganimate)
-df %>%
-  filter(area == "China") %>%
+
+top_6_total_cases <- df %>%
+  filter(area != "Others") %>%
+  group_by(area, date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
+  group_by(area) %>%
+  filter(sum(n_cases_per_day) > 0,
+         date == max(date)) %>%
+  arrange(-n_cases) %>%
+  head(6)
+
+plot_data <- df %>%
+  filter(area %in% top_6_total_cases$area) %>%
   group_by(area, date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
   mutate(percent_recovered = n_recovered / n_cases,
-         percent_deaths = n_deaths / n_cases) %>%
-  # filter(date == min(date)) %>%
+         percent_deaths = n_deaths / n_cases)
+
+anim <- plot_data %>%
   ggplot(aes(x = percent_recovered, y = percent_deaths)) +
-  geom_point() +
-    transition_time(date) +
-    ease_aes('linear')
-  
-  
+  geom_point(aes(colour=area, size=n_cases)) +
+  theme_custom  + theme(legend.title=element_text(size=10, family = "Source Sans Pro")) +
+  scale_y_continuous(labels = percent_format(accuracy = 1), limits=c(0, .1)) +
+  scale_x_continuous(labels = percent_format(accuracy = 1), limits=c(0, .6)) +
+  labs(title = 'COVID-19 Recovery/Mortality {frame_time}', x = 'recovered', y = 'mortality', 
+       size="cases", colour="") +
+  transition_time(date) +
+  ease_aes('linear') 
+
+animate(anim, width = 1600, height = 1600, res=200, fps=30, duration=10, end_pause = 5)
+anim_save("pics/anim_mortality_recovered.gif")
+
+# df_to_plot <- df %>%
+#   filter(area %in% top_6_total_cases$area) %>%
+#   group_by(area, date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
+#   mutate(percent_recovered = n_recovered / n_cases,
+#          percent_deaths = n_deaths / n_cases)
+# 
+# df_to_plot %>%
+#   group_by(area, date) %>% summarise_at(vars(starts_with("n_")), sum, na.rm=T) %>%
+#   mutate(percent_recovered = n_recovered / n_cases,
+#          percent_deaths = n_deaths / n_cases) %>%
+#   ggplot(aes(x = percent_recovered, y = percent_deaths)) +
+#   geom_line(aes(colour=area), alpha=.7) +
+#   geom_point(data=df_to_plot %>% filter(date == max(date)), aes(colour=area, size=n_cases)) +
+#   ggtitle("something") + xlab("recovered") + ylab("mortality") + labs(colour="") +
+#   scale_y_continuous(labels = percent_format(accuracy = 1)) +
+#   scale_x_continuous(labels = percent_format(accuracy = 1)) +
+#   ylim(0, .1) + xlim(0, .6) +
+#   theme_custom
+
+#### SPATIAL EVOLUTION ####
+# weighted average of lat/long over sphere
+com_df <- df %>%
+  mutate(lat = pi*lat/180, long = pi*long/180) %>%
+  group_by(lat, long, date) %>%
+  summarise(n_cases = sum(n_cases, na.rm=T)) %>%
+  mutate(x = cos(lat)*cos(long), y = sin(lat)*cos(long), z = sin(long)) %>%
+  group_by(date) %>%
+  summarise(com_x = sum(n_cases*x, na.rm = T) / sum(n_cases, na.rm = T),
+            com_y = sum(n_cases*y, na.rm = T) / sum(n_cases, na.rm = T),
+            com_z = sum(n_cases*z, na.rm = T) / sum(n_cases, na.rm = T),
+            n_cases = sum(n_cases, na.rm=T)) %>%
+  mutate(com_lat = (180/pi)*atan2(com_y, com_x)+180,
+         com_long = 180-(180/pi)*atan2(com_z, sqrt(com_x^2 + com_y^2)),
+         r = sqrt(com_x^2 + com_y^2 + com_z^2)) %>%
+  arrange(date)
+
+p_com_1 <- com_df %>%
+  ggplot(aes(x=com_long, y=com_lat)) +
+  geom_point(aes(size = n_cases, colour=date)) +
+  geom_path(arrow = arrow(type="closed")) +
+  theme(legend.position = "none") +
+  xlab("latitude °") + ylab("longitude °") + ggtitle("COVID-19 'centre of mass'") +
+  labs(size="cases", colour="date") +
+  theme_custom + theme(legend.position = "left",
+                       legend.title=element_text(size=10, family = "Source Sans Pro"))
+p_com_1
+p_com_2 <- com_df %>%
+  ggplot(aes(x=date, y=1-r)) +
+  geom_point(aes(size = n_cases, colour=date)) +
+  geom_line() +
+  theme(legend.position = "none") +
+  xlab("") + ylab("dispersion") + ggtitle("COVID-19 spatial dispersion") +
+  theme_custom + theme(legend.position="none")
+p_com_2
+p_com <- p_com_1 + p_com_2
+p_com
+
+ggsave("pics/p_com.png", device = "png", dpi="retina", width=300, height=200, units="mm")
+
+
+#### LEAFLET ####
+library(leaflet)
+library(htmltools)
+library(htmlwidgets)
+library(mapview)
+
+pal <- colorNumeric("Reds", domain = df$n_cases %>% unique())
+
+map <- df %>%
+  filter(day == max(day)) %>%
+  arrange(-n_cases) %>%
+  leaflet() %>%
+  setView(zoom=3, lat=30, lng=5) %>%
+  # addProviderTiles(providers$Stamen.Watercolor) %>%
+  addProviderTiles(providers$CartoDB.DarkMatter) %>%
+  addCircleMarkers(lng = ~long, lat = ~lat, radius = ~sqrt(n_cases)/5, stroke=T,
+                   opacity = .9, color="chocolate",
+                   fillColor = ~pal(n_cases), fillOpacity = .9)
+# maps
+mapshot(map, file = file.path(getwd(), "pics/map.png"),
+        vheight = 1000, vwidth = 2100)
+
+mapshot(map %>% addProviderTiles(providers$Stamen.Watercolor), 
+        file = file.path(getwd(), "pics/map2.png"),
+        vheight = 1000, vwidth = 2100)
+
